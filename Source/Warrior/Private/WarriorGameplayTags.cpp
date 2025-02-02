c7092612d312595d031d810ea8c05753dd856f06
Emilisa <emilisa90@gmail.com>
2025-02-02 13:23:11 +0700

    11. Native Gameplay Tags

---
+++ Source/Warrior/Private/WarriorGameplayTags.cpp
@@ -0,0 +1,7 @@
+#include "WarriorGameplayTags.h"
+
+namespace WarriorGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
+}
