2476067bb6e23331689c677c919eba380df7b418
Emilisa <emilisa90@gmail.com>
2025-12-02 21:52:33 +0700

    19. Ability Activation Policy

---
+++ Source/Warrior/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "WarriorGameplayAbility.generated.h"
+
+UENUM()
+enum class EWarriorAbilityActivationPolicy : uint8
+{
+	OnTriggered,
+	OnGiven
+};
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	
+	UPROPERTY(EditDefaultsOnly, Category = WarriorAbility)
+	EWarriorAbilityActivationPolicy AbilityActivationPolicy = EWarriorAbilityActivationPolicy::OnTriggered;
+};
