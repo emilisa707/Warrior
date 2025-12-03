31d429a7c69b41b79833796ada1428715ec805fb
Emilisa <emilisa90@gmail.com>
2025-12-03 22:06:11 +0700

    22. Start Up Data

---
+++ Source/Warrior/Public/DataAsset/StartUpData/DataAsset_StartUpDataBase.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DataAsset_StartUpDataBase.generated.h"
+
+class UWarriorAbilitySystemComponent;
+class UWarriorGameplayAbility;
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UDataAsset_StartUpDataBase : public UDataAsset
+{
+	GENERATED_BODY()
+
+	public:
+	virtual void GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InWarriorASCToGive, int32 ApplyLevel = 1);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "StartUpData")
+	TArray<TSubclassOf<UWarriorGameplayAbility>> ActivateOnGivenAbilities;
+
+	UPROPERTY(EditDefaultsOnly, Category = "StartUpData")
+	TArray<TSubclassOf<UWarriorGameplayAbility>> ReactiveAbilities;
+
+	void GrantAbilities(const TArray<TSubclassOf<UWarriorGameplayAbility>>& InAbilitiesToGive, UWarriorAbilitySystemComponent* InWarriorASCToGive, int32 ApplyLevel = 1);
+	
+};
