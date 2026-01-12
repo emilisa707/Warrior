4369efd6e4bffd246c63b6c8db00e6f42cbd3d22
Emilisa <emilisa90@gmail.com>
2026-01-12 20:40:17 +0700

    update 5.7

--- Source/Warrior.Target.cs
+++ Source/Warrior.Target.cs
@@ -9,7 +9,7 @@ public class WarriorTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
 		ExtraModuleNames.Add("Warrior");
 	}
 }
