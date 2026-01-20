ff34c16a1114a92dead7f32105eda547e1c37845
Emilisa <emilisa90@gmail.com>
2026-01-20 22:08:20 +0700

    57. Attribute Set

--- Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
+++ Source/Warrior/Private/AbilitySystem/WarriorAttributeSet.cpp
@@ -1,2 +1,12 @@
 #include "AbilitySystem/WarriorAttributeSet.h"
 
+
+UWarriorAttributeSet::UWarriorAttributeSet()
+{
+	InitCurrentHealth(100.f);
+	InitMaxHealth(100.f);
+	InitCurrentRage(0.f);
+	InitMaxRage(100.f);
+	InitAttackPower(20.f);
+	InitDefensePower(5.f);
+}
