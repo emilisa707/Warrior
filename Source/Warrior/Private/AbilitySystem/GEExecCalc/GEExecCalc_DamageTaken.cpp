0424fb0c52855b57b03441bc08ceb36b53f1d222
Emilisa <emilisa90@gmail.com>
2026-01-28 22:03:31 +0700

    72. Calculate Final Damage Done

--- Source/Warrior/Private/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
+++ Source/Warrior/Private/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.cpp
@@ -1,5 +1,6 @@
 #include "AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.h"
 
+#include "WarriorDebugHelper.h"
 #include "WarriorGameplayTags.h"
 #include "AbilitySystem/WarriorAttributeSet.h"
 
@@ -7,11 +8,13 @@ struct  FWarriorDamageCapture
 {
 	DECLARE_ATTRIBUTE_CAPTUREDEF(AttackPower)
 	DECLARE_ATTRIBUTE_CAPTUREDEF(DefensePower)
+	DECLARE_ATTRIBUTE_CAPTUREDEF(DamageTaken)
 
 	FWarriorDamageCapture()
 	{
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, AttackPower, Source, false)
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, DefensePower, Target, false)
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UWarriorAttributeSet, DamageTaken, Target, false)
 	}
 };
 
@@ -38,6 +41,7 @@ UGEExecCalc_DamageTaken::UGEExecCalc_DamageTaken()
 
 	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().AttackPowerDef);
 	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().DefensePowerDef);
+	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().DamageTakenDef);
 }
 
 void UGEExecCalc_DamageTaken::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -55,8 +59,9 @@ void UGEExecCalc_DamageTaken::Execute_Implementation(const FGameplayEffectCustom
 	EvaluateParameters.TargetTags = EffectSpec.CapturedTargetTags.GetAggregatedTags();
 
 	float SourceAttackPower = 0.f;
-
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetWarriorDamageCapture().AttackPowerDef, EvaluateParameters, SourceAttackPower);
+	Debug::Print(TEXT("Source Attack Power"), SourceAttackPower);
+	
 	float BaseDamage = 0.f;
 	int32 UsedLightAttackComboCount = 0;
 	int32 UsedHeavyAttackComboCount = 0;
@@ -65,19 +70,47 @@ void UGEExecCalc_DamageTaken::Execute_Implementation(const FGameplayEffectCustom
 		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Shared_SetByCaller_BaseDamage))
 		{
 			BaseDamage = TagMagnitude.Value;
+			Debug::Print(TEXT("Base Damage"), BaseDamage);
 		}
 		
 		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Player_SetByCaller_AttackType_Light))
 		{
 			UsedLightAttackComboCount = TagMagnitude.Value;
+			Debug::Print(TEXT("UsedLightAttackComboCount"), UsedLightAttackComboCount);
 		}
 
 		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Player_SetByCaller_AttackType_Heavy))
 		{
 			UsedHeavyAttackComboCount = TagMagnitude.Value;
+			Debug::Print(TEXT("UsedHeavyAttackComboCount"), UsedHeavyAttackComboCount);
 		}
 	}
 
 	float TargetDefensePower = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetWarriorDamageCapture().DefensePowerDef, EvaluateParameters, TargetDefensePower);
+	Debug::Print(TEXT("TargetDefensePower"), TargetDefensePower);
+	if (UsedLightAttackComboCount != 0)
+	{
+		const float DamageIncreasePercentLight = (UsedLightAttackComboCount - 1) * 0.05 + 1.f;
+		BaseDamage *= DamageIncreasePercentLight;
+		Debug::Print(TEXT("BaseDamage"), BaseDamage);
+	}
+	
+	if (UsedHeavyAttackComboCount != 0)
+	{
+		const float DamageIncreasePercentHeavy = UsedHeavyAttackComboCount * 0.15 + 1.f;
+		BaseDamage *= DamageIncreasePercentHeavy;
+		Debug::Print(TEXT("BaseDamage"), BaseDamage);
+	}
+
+	const float FinalDamageDone = BaseDamage * SourceAttackPower / TargetDefensePower;
+	Debug::Print(TEXT("FinalDamageDone"), FinalDamageDone);
+	if (FinalDamageDone > 0.f)
+	{
+		OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(
+			GetWarriorDamageCapture().DamageTakenProperty,
+			EGameplayModOp::Override,
+			FinalDamageDone
+		));
+	}
 }
--- Source/Warrior/Public/AbilitySystem/WarriorAttributeSet.h
+++ Source/Warrior/Public/AbilitySystem/WarriorAttributeSet.h
@@ -45,4 +45,8 @@ public:
 	UPROPERTY(BlueprintReadOnly, Category = "Damage")
 	FGameplayAttributeData DefensePower;
 	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet,DefensePower)
+
+	UPROPERTY(BLueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData DamageTaken;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet,DamageTaken)
 };
