d45b92bf146bf21523e5a51e99408b0f84d43483
Emilisa <emilisa90@gmail.com>
2026-01-28 21:32:33 +0700

    71. Retrieve Hero Damage Info

--- Source/Warrior/Public/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.h
+++ Source/Warrior/Public/AbilitySystem/GEExecCalc/GEExecCalc_DamageTaken.h
@@ -14,4 +14,6 @@ class WARRIOR_API UGEExecCalc_DamageTaken : public UGameplayEffectExecutionCalcu
 
 public:
 	UGEExecCalc_DamageTaken();
+
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, OUT FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
 };
